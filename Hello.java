92b0e1bdd4be324740492643f1a8813889e2d8a3
Leesanghun99 <dldn99022@naver.com>
2024-08-06 11:31:23 +0900

    Add files via upload
    첫번째 예제

---
+++ Hello.java
@@ -0,0 +1,29 @@
+package day01; //패키지(클래스를 묶는 단위) = 꾸러미
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*여러줄 주석*/
+		/**
+		 * 작성일:
+		 * 작성자:
+		 * 내용:
+		 */
+		System.out.print("Hello Java \n Hello Java \nHello Java");
+		/*
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		*/
+
+	}
+
+}
