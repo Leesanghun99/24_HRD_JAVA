dd47b4d1f60a7c0d67ee9f07b58f14c849ee1bc7
Leesanghun99 <dldn99022@naver.com>
2024-09-04 16:22:58 +0900

    Add files via upload

---
+++ Ex001.py
@@ -0,0 +1,10 @@
+print(273, 10, "Python")
+print()
+print()
+age = 26
+print("나는", "이상헌", "이다")
+print(f"나는 이상헌 이고 나이는 {age}")
+
+a = 1
+
+
---
+++ Ex003.py
@@ -0,0 +1,17 @@
+#name = input("너의 이름은? ")
+#print(f"너의 이름은:  {name}")
+
+# number1 = input("첫번째 값은? ")
+# number2 = input("두번째 값은? ")
+# print(int(number1) + int(number2))
+
+# hp1 = '10.2'
+# hp2 = '52.2'
+# hp3 = '-10.3'
+# print(hp1, hp2, hp3, sep=' ')
+
+a = 77
+b = '자전거'
+c = 3.373777
+d = 90
+print('%d, %s, %.2f, %d%%. %6s, %5d'%(a,b,c,d,b,a))
